9109bfa592b35162169f0729d82a5438b7553f5f
Beca Galliano <beca.galliano@digital.cabinet-office.gov.uk>
2026-09-14 17:36:59 +0100

    PSREDEV-3696: Clean up
    - Remove helper and use pytest convention conftest
    - Refactor iam_pass_role_arn.py

--- test/test_iam_pass_role/test_iam_pass_role_arn.py
+++ test/test_iam_pass_role/test_iam_pass_role_arn.py
@@ -1,7 +1,7 @@
 import unittest
 from pathlib import Path
 
-from helpers import run_check
+from conftest import run_check
 
 FIXTURES = Path(__file__).resolve().parent / "fixtures"
 CHECK_ID = "GDS_DEVPLATFORM_001"
